617695aa00198e0e3a31a404a167d9c9ff126571
oury <oury@DESKTOP-L4DHVK0>
2017-11-24 03:27:12 +0800

    17_11_24_03_26 draw the plant

--- test2/values.h
+++ test2/values.h
@@ -2,5 +2,19 @@
 #define _VALUES_
 
 static int width = 800, height = 600;
+static float r0 = 0.0f, g0 = 1.0f, b0 = 1.0f; //sky blue--wall
+static float r1 = 0.5f, g1 = 0.5f, b1 = 0.5f; //gray--smoke
+static float r2 = 1.0f, g2 = 0.0f, b2 = 0.0f; //red--fire
+static float r3 = 1.0f, g3 = 1.0f, b3 = 1.0f; //white--human
+static float r4 = 0.0f, g4 = 0.0f, b4 = 1.0f; //blue
+static float r5 = 0.0f, g5 = 1.0f, b5 = 0.0f; //green
+static float r6 = 1.0f, g6 = 1.0f, b6 = 0.0f; //yellow
+static float r7 = 1.0f, g7 = 0.0f, b7 = 1.0f; //purple
+static float r8 = 0.5f, g8 = 0.5f, b8 = 0.0f; //drak yellow
+static float r9 = 1.0f, g9 = 0.5f, b9 = 0.0f; //orange
+static float r10 = 1.0f, g10 = 0.5f, b10 = 0.5f; //pink
+static float r11 = 0.0f, g11 = 0.5f, b11 = 0.5f; //dark blue
+static float r12 = 0.5f, g12 = 1.0f, b12 = 0.5f; //light green
+
 
 #endif
